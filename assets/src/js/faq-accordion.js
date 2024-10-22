78cf6c067c7a272c66485d75df819aef0637f91c
Kashkumar <kash.singh1989@gmail.com>
2024-10-22 16:07:58 +0100

    Updated  plugin files to enhance feature like tabs and javascript ajax

--- assets/src/js/faq-accordion.js
+++ assets/src/js/faq-accordion.js
@@ -1,8 +1,52 @@
 import '../scss/faq-styles.scss';
+
 jQuery(document).ready(function($) {
-    $('.faq-item__title').on('click', function() {
-        var $content = $(this).next('.faq-item__content');
-        $('.faq-item__content').not($content).slideUp(); // Close other open items
+    // Handle tab switching
+    $('.faq__tab-link').on('click', function(e) {
+        e.preventDefault();
+
+        // Remove active class from all tabs and hide all content
+        $('.faq__tab-link').removeClass('active');
+        $('.faq__item__content').hide(); // Hide all content first
+
+        // Add active class to the clicked tab
+        $(this).addClass('active');
+
+        // Get the target content ID from the data attribute instead of href
+        const category = $(this).data('category');
+        $('#faq-list').data('category', category); // Set the category data for the FAQ list
+
+        // Fetch FAQs based on selected category
+        fetchFAQs(category);
+    });
+
+    // Function to fetch FAQs via AJAX
+    function fetchFAQs(category) {
+        $.ajax({
+            url: sfmData.ajaxUrl,
+            type: 'POST',
+            data: {
+                action: 'fetch_faqs',
+                category: category,
+                nonce: sfmData.nonce
+            },
+            success: function(response) {
+                if (response.success) {
+                    $('#faq-list').html(response.data);
+                } else {
+                    $('#faq-list').html('<p>Error retrieving FAQs.</p>'); // Static error message
+                }
+            },
+            error: function() {
+                $('#faq-list').html('<p>Error retrieving FAQs.</p>');
+            }
+        });
+    }
+
+    // Handle FAQ accordion toggle
+    $(document).on('click', '.faq__item__title', function() {
+        var $content = $(this).next('.faq__item__content');
+        $('.faq__item__content').not($content).slideUp(); // Close other open items
         $content.slideToggle(); // Toggle the clicked item
     });
 });
--- includes/class-shortcode-faq-manager.php
+++ includes/class-shortcode-faq-manager.php
@@ -1,7 +1,6 @@
 <?php
 class Shortcode_FAQ_Manager
 {
-
     /**
      * Initializes the shortcode and related hooks.
      */
@@ -9,6 +8,8 @@ class Shortcode_FAQ_Manager
     {
         add_shortcode('faqs', [$this, 'render_faq_shortcode']);
         add_action('wp_enqueue_scripts', [$this, 'enqueue_assets']);
+        add_action('wp_ajax_fetch_faqs', [$this, 'fetch_faqs']);
+        add_action('wp_ajax_nopriv_fetch_faqs', [$this, 'fetch_faqs']);
     }
 
     /**
@@ -39,12 +40,58 @@ class Shortcode_FAQ_Manager
     {
         $atts = shortcode_atts([
             'count' => -1, // Default: show all FAQs
+            'category' => '', // Default: show all categories
         ], $atts, 'faqs');
 
+        // Get all FAQ categories for the tabs
+        $categories = get_terms(['taxonomy' => 'faq_category', 'hide_empty' => true]);
+
+        ob_start();
+        ?>
+        <div class="faq">
+            <div class="faq__tabs">
+                <ul class="faq__tab-list">
+                    <li class="faq__tab-item">
+                        <a href="#" class="faq__tab-link active" data-category="all"><?php esc_html_e('All', 'shortcode-faq-manager'); ?></a>
+                    </li>
+                    <?php foreach ($categories as $category): ?>
+                        <li class="faq__tab-item">
+                            <a href="#" class="faq__tab-link" data-category="<?php echo esc_attr($category->slug); ?>"><?php echo esc_html($category->name); ?></a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+            <div class="faq__list" id="faq-list">
+                <?php echo $this->get_faqs($atts['category'], $atts['count']); ?>
+            </div>
+        </div>
+        <?php
+        return ob_get_clean();
+    }
+
+    /**
+     * Retrieves FAQs based on the specified category and count.
+     * 
+     * @param string $category The category slug to filter FAQs.
+     * @param int $count The number of FAQs to show.
+     * @return string
+     */
+    private function get_faqs($category, $count)
+    {
         $args = [
             'post_type' => 'faq',
-            'posts_per_page' => intval($atts['count']),
+            'posts_per_page' => intval($count),
         ];
+
+        if ($category && $category !== 'all') {
+            $args['tax_query'] = [
+                [
+                    'taxonomy' => 'faq_category',
+                    'field'    => 'slug',
+                    'terms'    => $category,
+                ],
+            ];
+        }
 
         $faqs = new WP_Query($args);
 
@@ -53,20 +100,32 @@ class Shortcode_FAQ_Manager
         }
 
         ob_start();
-        ?>
-        <div class="faq-list">
-            <?php while ($faqs->have_posts()):
-                $faqs->the_post(); ?>
-                <div class="faq-item">
-                    <h3 class="faq-item__title"><?php echo esc_html(get_the_title()); ?></h3>
-                    <div class="faq-item__content" style="display: none;">
-                        <?php echo wp_kses_post(get_the_content()); ?>
-                    </div>
+        while ($faqs->have_posts()) {
+            $faqs->the_post();
+            ?>
+            <div class="faq__item">
+                <h3 class="faq__item__title"><?php echo esc_html(get_the_title()); ?></h3>
+                <div class="faq__item__content" style="display: none;">
+                    <?php echo wp_kses_post(get_the_content()); ?>
                 </div>
-            <?php endwhile; ?>
-        </div>
-        <?php
+            </div>
+            <?php
+        }
         wp_reset_postdata();
         return ob_get_clean();
+    }
+
+    /**
+     * Fetch FAQs via AJAX based on category.
+     */
+    public function fetch_faqs()
+    {
+        check_ajax_referer('sfm_nonce', 'nonce');
+
+        $category = sanitize_text_field($_POST['category']);
+        $count = -1; // Default to show all FAQs
+
+        $faqs = $this->get_faqs($category, $count);
+        wp_send_json_success($faqs);
     }
 }
